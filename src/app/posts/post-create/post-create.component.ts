a0ec794751be529356237e062d776dba407a5a8b
ibrahim <ibrahim.mowafy10@gmail.com>
2022-02-16 14:34:18 +0200

    Create post-create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
+  newPost = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onAddPost(postInput: HTMLTextAreaElement) {
+    this.newPost = postInput.value;
   }
-
 }
