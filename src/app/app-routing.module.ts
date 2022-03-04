308ee8731eade7817fc6baecc1b30f0078483cfc
ibrahim <ibrahim.mowafy10@gmail.com>
2022-03-04 19:30:27 +0200

    adding route to login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './auth/login/login.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { NgModule } from '@angular/core';
@@ -5,8 +6,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent   },
-  { path: 'edit/:postId', component: PostCreateComponent   },
+  { path: 'create', component: PostCreateComponent },
+  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
