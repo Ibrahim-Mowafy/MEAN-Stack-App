d1fc38f237ce146b35dec5270eae520f9f483b8a
ibrahim <ibrahim.mowafy10@gmail.com>
2022-03-05 22:23:08 +0200

    create signup route (Backend)

---
+++ backend/routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+const router = express.Router();
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = new User({
+      email: req.body.email,
+      password: hash,
+    });
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({ message: "User created!", result: result });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: error });
+      });
+  });
+});
+
+module.exports = router;
