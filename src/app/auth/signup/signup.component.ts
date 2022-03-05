a7a522f6a138aafafb02e89d74a4b731e3b8b843
ibrahim <ibrahim.mowafy10@gmail.com>
2022-03-05 20:38:44 +0200

    adding sinup form to component

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,18 @@
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  isLoading = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSignup(form: NgForm) {
+    console.log(form.value);
   }
-
 }
