ff85ff68ddd760bcf063e5ea882136ddb4b1101e
ibrahim <ibrahim.mowafy10@gmail.com>
2022-03-07 23:53:19 +0200

    reflect changes in ui after user login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './../auth/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  private authListenerSub!: Subscription;
+  userIsAuthenticated = false;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authListenerSub = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
   }
-
+  onLogout() {
+    this.authService.logout();
+  }
+  ngOnDestroy(): void {
+    this.authListenerSub.unsubscribe();
+  }
 }
