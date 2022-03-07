f43f122db55e05030885b2852bb29ec810a6c7fb
ibrahim <ibrahim.mowafy10@gmail.com>
2022-03-07 23:57:05 +0200

    adding function logout *navigate after user login *retern token after login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 import { AuthData } from './auth-data.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -6,7 +8,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
+  private token: string | undefined | null;
+  private isAuthenticated = false;
+  private authStatusListener = new Subject<boolean>();
+
+  getToken() {
+    return this.token;
+  }
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -22,13 +39,29 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post('http://localhost:3000/api/user/login', authData).subscribe(
-      (response) => {
-        console.log(response);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    this.http
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          const token = response.token;
+          this.token = token;
+          if (token) {
+            this.authStatusListener.next(true);
+            this.isAuthenticated = true;
+            this.router.navigate(['/']);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
